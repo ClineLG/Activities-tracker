8891985b85593fb87cf7a5bd2b898fc1747df9ae
céline leguelvouit <celin_tfx35@MacBook-Pro.local>
2025-02-05 17:26:22 +0100

    add activity routes and endpoints for daily, weekly, monthly, and yearly activity tracking

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,13 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI);
 
 const userRoutes = require("./routes/user");
-app.use(userRoutes);
+app.use("/user", userRoutes);
+
+const activityRoutes = require("./routes/activity");
+app.use("/activity", activityRoutes);
+
+const activitiesRoute = require("./routes/activities");
+app.use("/activities", activitiesRoute);
 
 app.get("/", (req, res) => {
   res.status(201).json("welcom on the app");
